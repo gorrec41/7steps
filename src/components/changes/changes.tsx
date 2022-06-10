3b1472add611bae6d2bc279fbe4877959e9c6979
gorrec41 <batrak1@ya.ru>
2022-06-10 23:44:48 +0300

    section3

---
+++ src/components/changes/changes.tsx
@@ -0,0 +1,13 @@
+import st from './changes.module.scss'
+
+
+function Сhanges() {
+    return (
+        <header className={st.changes}>
+            <div className='container'>
+
+            </div>
+        </header>
+    )
+}
+export default Сhanges;
